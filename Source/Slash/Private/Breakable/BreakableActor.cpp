e580ea0046c218c83d24494bcf808b0846b71001
huzi2 <huzi2@naver.com>
2023-08-07 22:07:04 +0900

    부술 수 있는 객체를 부쉈을 때 보물이 나오도록 함. 보물 클래스 추가

--- Source/Slash/Private/Breakable/BreakableActor.cpp
+++ Source/Slash/Private/Breakable/BreakableActor.cpp
@@ -3,8 +3,11 @@
 
 #include "Breakable/BreakableActor.h"
 #include "GeometryCollection/GeometryCollectionComponent.h"
+#include "Items/Treasure.h"
+#include "Components/CapsuleComponent.h"
 
 ABreakableActor::ABreakableActor()
+	: bBroken(false)
 {
 	PrimaryActorTick.bCanEverTick = false;
 
@@ -12,11 +15,29 @@ ABreakableActor::ABreakableActor()
 	SetRootComponent(GeometryCollection);
 	GeometryCollection->SetGenerateOverlapEvents(true);
 	GeometryCollection->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+	GeometryCollection->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+	Capsule->SetupAttachment(GetRootComponent());
+	Capsule->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	Capsule->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Block);
 }
 
 void ABreakableActor::GetHit_Implementation(const FVector& ImpactPoint)
 {
+	if (bBroken) return;
+	
+	bBroken = true;
 
+	UWorld* World = GetWorld();
+	if (World && TreasureClasses.Num() > 0)
+	{
+		FVector Location = GetActorLocation();
+		Location.Z += 75.f;
+
+		const int32 Selection = FMath::RandRange(0, TreasureClasses.Num() - 1);
+		World->SpawnActor<ATreasure>(TreasureClasses[Selection], Location, GetActorRotation());
+	}
 }
 
 void ABreakableActor::BeginPlay()
--- Source/Slash/Public/Breakable/BreakableActor.h
+++ Source/Slash/Public/Breakable/BreakableActor.h
@@ -8,6 +8,8 @@
 #include "BreakableActor.generated.h"
 
 class UGeometryCollectionComponent;
+class ATreasure;
+class UCapsuleComponent;
 
 UCLASS()
 class SLASH_API ABreakableActor : public AActor, public IHitInterface
@@ -23,7 +25,17 @@ public:
 private:
 	virtual void BeginPlay() override;
 
-private:
-	UPROPERTY(VisibleAnywhere)
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	UGeometryCollectionComponent* GeometryCollection;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UCapsuleComponent* Capsule;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Breakable Properties")
+	TArray<TSubclassOf<ATreasure>> TreasureClasses;
+
+private:
+	bool bBroken;
 };
